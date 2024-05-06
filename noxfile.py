1d7e7bfbc981d876376ceca5716435a5070b3bfe
Leandro Lucarella <luca-frequenz@llucax.com>
2024-05-06 13:47:27 +0200

    Tweak noxfile and tools to account for the generated `src`
    We need to set the sources explicitly to an empty list to avoid many
    checks to run on the generate code that would otherwise fail.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,10 @@
 
 """Configuration file for nox."""
 
-from frequenz.repo.config import RepositoryType, nox
+from frequenz.repo.config import nox
+from frequenz.repo.config.nox import default
 
-nox.configure(RepositoryType.LIB)
+config = default.lib_config.copy()
+# We don't really have a source per-se, as all code in "src" is generated.
+config.source_paths = []
+nox.configure(config)
